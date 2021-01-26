a1d306c00baf776303ba785b205ff5e4c41bb342
alvaro <aaruizf@unal.edu.co>
2021-01-25 22:11:29 -0500

    Modify posts controller to add before_action and new and create methods

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,23 @@
+class PostsController < ApplicationController
+  before_action :require_login, only: [:new, :create]
+
+
+  def new
+    @post = Post.new
+  end
+  
+  def create; end
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to new_login_url # halts request cycle
+    end
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+end
