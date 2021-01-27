0c65fed06fd66f4f18a12c230b555ccf2bfea84c
alvaro <aaruizf@unal.edu.co>
2021-01-27 18:44:25 -0500

    Modify posts controller to change action when returning to deleted post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -56,7 +56,13 @@ class PostsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_post
-    @post = Post.find(params[:id])
+    if @post
+      @post = Post.find(params[:id])
+    else
+      @post = Post.new
+      render 'new'
+    end
+
   end
 
   # Only allow a list of trusted parameters through.
