ba8cd2a3134555f2f21f0144cc6a2c0d80a2fa98
alvaro <aaruizf@unal.edu.co>
2021-01-27 18:29:35 -0500

    Fix issue while authenticating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,6 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # attr_accessor :name, :email #, :password, :password_confirmation, :encrypted_password
-  validates :name, presence: true, length: { maximum: 250 }, uniqueness: true
   validates :password, length: { minimum: 6 }
-  #has_secure_password
   has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2021_01_26_214150) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "password"
     t.datetime "created_at", precision: 6, null: false
