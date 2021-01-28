be281ece1a365a093ff3d5cc6c92f85ab91b2343
alvaro <aaruizf@unal.edu.co>
2021-01-28 17:29:59 -0500

    Modify posts view to reduce logic in it

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def show_edit_post(post)
+    link_to 'Edit', edit_post_path(post)
+  end
+
+  def show_destroy_post(post)
+    link_to 'Destroy', post, method: :delete, data: { confirm: 'Are you sure?' }
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,17 +1,5 @@
 module PostsHelper
-  def show_posts
-    if user_signed_in?
-      render 'tweets/profile'
-    else
-      render 'tweets/trends'
-    end
-  end
-
   def show_create_post_link
-    if user_signed_in?
-      render 'tweets/profile'
-    else
-      render 'tweets/trends'
-    end
+    link_to 'Create a Post', new_post_path if user_signed_in?
   end
 end
