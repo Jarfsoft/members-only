6f9497d930d3e03eac4986f5d598aeb846e7a794
alvaro <aaruizf@unal.edu.co>
2021-01-28 16:14:36 -0500

    Modify application layout, according to TSE's suggestion

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,17 @@
 module PostsHelper
+  def show_posts
+    if user_signed_in?
+      render 'tweets/profile'
+    else
+      render 'tweets/trends'
+    end
+  end
+
+  def show_create_post_link
+    if user_signed_in?
+      render 'tweets/profile'
+    else
+      render 'tweets/trends'
+    end
+  end
 end
